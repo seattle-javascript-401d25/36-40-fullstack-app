03e6368fbd7041c14c1e8edb88faafe916e88e1e
Noah Israel <noahmvf@gmail.com>
2018-08-05 14:42:17 -0700

    dashboard

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Dashboard extends React.Component {
+import ImageForm from '../image-form/image-form';
+import createImageApiRequest from '../../actions/image';
+
+const mapDispatchToProps = dispatch => ({
+  createImage: image => dispatch(createImageApiRequest(image)),
+});
+
+
+class Dashboard extends React.Component {
   render() {
+    const { createImage } = this.props;
     return (
       <div className="dashboard">
         <h1>Hello from Dashboard</h1>
+        <ImageForm onComplete={ createImage } />
       </div>
     );
   }
 }
+
+Dashboard.propTypes = {
+  createImage: PropTypes.func,
+};
+
+export default connect(null, mapDispatchToProps)(Dashboard);
